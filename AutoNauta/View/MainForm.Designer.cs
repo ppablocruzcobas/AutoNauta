58d810f9d0b3865613572f189b23b951d1e3eae1
Pedro Pablo <pedro.cruz@isucorp.ca>
2021-04-13 03:10:32 -0400

    Set timer every 60 secs.

--- AutoNauta/View/MainForm.Designer.cs
+++ AutoNauta/View/MainForm.Designer.cs
@@ -156,7 +156,7 @@
             // timer
             // 
             this.timer.Enabled = true;
-            this.timer.Interval = 5000;
+            this.timer.Interval = 60000;
             this.timer.Tick += new System.EventHandler(this.timer_Tick);
             // 
             // MainForm
