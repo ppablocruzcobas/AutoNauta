599e965708e3ab6d7f256142fb8d14edb13e7831
Pedro Pablo <pedro.cruz@isucorp.ca>
2021-06-10 19:21:47 -0400

    Add packages to the Crypt engine.

---
+++ AutoNauta/Util/CryptEngine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using Effortless.Net.Encryption;
+
+namespace AutoNauta.Util
+{
+    class CryptEngine
+    {
+        public static string Encrypt(string input)
+        {
+            return Strings.Encrypt(input, null, null);
+        }
+        public static string Decrypt(string input)
+        {
+            return Strings.Decrypt(input, null, null);
+        }
+    }
+}
